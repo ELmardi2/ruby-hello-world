c89c4c10058d31d32ce21673834f9ae91cd032cf
mardi yahia <mardi.yahia@gmail.com>
2016-05-26 10:59:00 +0200

    almardi

--- hello_world.rb
+++ hello_world.rb
@@ -1,7 +1,16 @@
 class HelloWorld
    def self.hello
-       #
-       # YOUR CODE GOES HERE
-       #
-   end
-end 
+      class HelloWorld
+  def self.hello
+     class HelloWorld
+  def initialize(name)
+     @name = name.capitalize
+  end
+  def sayHi
+     puts "Hello #{@name}!"
+  end
+endhello = HelloWorld.new("World")
+hello.sayHi
+  end
+end
+  
